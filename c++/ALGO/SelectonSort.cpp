0f0f746bd4e002895c2d84ec58317b73bf9d93c0
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-07-26 21:03:23 +0530

    algo selection sort

---
+++ c++/ALGO/SelectonSort.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+#include<conio.h>
+
+using namespace std;
+int MIN(int A[],int K,int Klen);
+
+
+int main (){
+int a[] ={1,56,98,2,65,89,20,99,69,89};
+int Klen=sizeof(a)/sizeof(a[0]);
+int temp,LOC,K;
+  for(K=0;K<=Klen-2;K++){            /// N-2 because K=0 have to loop upto index of (n-2) value (NUMER OF ELEMENT - 1 => means  TOTAL INDEX(start from 0 )  and again - 1 => FOR comparing value of K, with last value of  array )
+       LOC=MIN(a,K,Klen);       // MIN(array,K,length of array) => RETURN INDEX OF SMMALEST VALUE IN ARRAY
+      // swaping area
+        temp=a[K];
+        a[K]=a[LOC];
+        a[LOC]=temp;
+   }
+   for(K=0;K<=9;K++)
+     cout<<a[K]<<"  ";
+
+    getch();
+    return 0;
+}
+
+int MIN(int A[],int K,int Klen)
+{
+    int j,LOC,MIN;
+    MIN=A[K];    // VALUE OF ARRAY AT INDEX NO  K IS INITIALIZE WITH-- MIN
+    LOC=K;       // INDEX NO OF K IS INITIALIZE
+
+       for(j=K+1;j<=Klen-1;j++)   // loops upto last index no from second index of array ,, first index is k=0 ,
+            if(MIN>A[j]){
+                 MIN=A[j];     // MIN BECOME LOWEST VALUE (LOWEST VALUE MEANS  UPTO WHICH INDEX J  LOOP REVLOVE)
+                  LOC=j;      // SAVE THE INDEX OF THE LOWEST VALUE
+            }
+        return LOC;
+}
+
+
---
+++ js exersise/01Algo/SelectionSort.js
@@ -0,0 +1,29 @@
+let a =[1,56,98,2,65,89,20,99,69,89];
+let temp,LOC,K;
+  for(K=0;K<=a.length-2;K++){            /// a.length-2 because K=0 have to loop upto index of (a.lemgth-2) value (NUMER OF ELEMENT - 1 => means  TOTAL INDEX(start from 0 )  and again - 1 => FOR comparing value of K, with last value of  array )
+       LOC=MIN(a,K,a.length);       // MIN(array,K,length of array) => RETURN INDEX OF SMMALEST VALUE IN ARRAY
+      // swaping area
+        temp=a[K];
+        a[K]=a[LOC];
+        a[LOC]=temp;
+   }
+   for(K=0;K<=9;K++)
+ {    console.log(a[K])
+}
+
+
+function   MIN(A,K,Klen)
+{
+    let j,LOC,MIN;
+    MIN=A[K];    // VALUE OF ARRAY AT INDEX NO  K IS INITIALIZE WITH-- MIN
+    LOC=K;       // INDEX NO OF K IS INITIALIZE
+
+       for(j=K+1;j<=Klen-1;j++)   // loops upto last index no from second index of array ,, first index is k=0 ,
+            if(MIN>A[j]){
+                 MIN=A[j];     // MIN BECOME LOWEST VALUE (LOWEST VALUE MEANS  UPTO WHICH INDEX J  LOOP REVLOVE)
+                  LOC=j;      // SAVE THE INDEX OF THE LOWEST VALUE
+            }
+        return LOC;
+}
+
+
