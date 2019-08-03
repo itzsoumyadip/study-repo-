0b1f940edc395e1d36f4bacb7e03af81317948aa
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-03 20:35:10 +0530

    SingleQuoteString

---
+++ PHP/P code/SingleQuotedString.php
@@ -0,0 +1,14 @@
+<?php
+  
+<?php
+  
+
+
+$currency  = 'penny';
+
+$sampleString = 'A $currency we svae is $currency  we  earned  ';
+echo $sampleString;  /// A $currency we svae is $currency we earned
+
+  ?>
+  
+  ?>
