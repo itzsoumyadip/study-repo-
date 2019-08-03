4d9b032c96f9a68abbb6343d92b6410a72111026
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-03 20:48:03 +0530

    if we use stringQuote string can not  print in new  line with the use of \n

--- PHP/P code/SingleQuotedprintBlackSlash n Asits.php
+++ PHP/P code/SingleQuotedprintBlackSlash n Asits.php
@@ -4,6 +4,6 @@
 
 $name  = 'soumyadip\'s and \n this will be new line (but will be not due to single quote) ';
 
-echo $name; //\n will be printed as it is  
+echo $name; //\n will be printed As it is  
 
   ?>
