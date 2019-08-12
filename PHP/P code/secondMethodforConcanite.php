40355e24d94171c5861d8673714388da574bd010
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-12 13:44:12 +0530

    double string 2nd concatination method

---
+++ PHP/P code/secondMethodforConcanite.php
@@ -0,0 +1,11 @@
+<?php
+  
+$currency  = 'penny';
+$sampleString = "A $currency we svae is $currency  we  earned  ";
+  ///A penny we svae is penny we earned
+echo $currency .$sampleString; 
+ echo "<br>".$currency ."A $currency we svae is $currency  we  earned  "; // this .also work with single quote
+  
+  
+  ?>
+</body>
