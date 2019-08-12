b83ee843ffc796ed50d99a795e1672c1b675c895
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-12 13:38:17 +0530

    double string 1st concatination method

--- PHP/P code/DoubleQuotedString.php
+++ PHP/P code/DoubleQuotedString.php
@@ -3,4 +3,9 @@
 $currency  = 'penny';
 $sampleString = "A $currency we svae is $currency  we  earned  ";
 echo $sampleString;  ///A penny we svae is penny we earned
+  
+ echo " \n {$currency} first concatinate  method "; 
+  
+  
+  
   ?>
