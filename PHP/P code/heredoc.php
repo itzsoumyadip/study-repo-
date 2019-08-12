9d5da7667426567152e5844718213c58d9fb07ef
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-12 13:32:10 +0530

    heredoc.php

---
+++ PHP/P code/heredoc.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Online PHP Script Execution</title>
+</head>
+<body>
+<?php
+echo <<<EOT
+     this is a heredoc  example 
+	 you see how it is write 
+	 text will be printed as it is
+	 writen here
+                   -SOUMYADIP SAHA
+EOT; 
+?>
+</body>
+</html>
