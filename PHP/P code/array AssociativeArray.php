0e261bc810a1b91e09995f8196096680405a9a18
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-14 14:55:32 +0530

    associative array in php

---
+++ PHP/P code/array AssociativeArray.php
@@ -0,0 +1,12 @@
+<?php
+ $aurthor = array(
+      "quarky"=> "Charles",
+	  "see its key value",
+	  10=> "store",
+	  "briliant"=>"Soumyadip",
+       "see its key value" 
+ );
+  
+  print_r($aurthor); // Array ( [quarky] => Charles [0] => see its key value [10] => store [briliant] => Soumyadip [11] => see its key value )
+  
+?>
