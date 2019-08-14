cc3cf6741318fc861c529d5295722028eba310c8
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-14 11:19:43 +0530

    array define methods in php

---
+++ PHP/P code/arraytypes.php
@@ -0,0 +1,17 @@
+/* array define types  */ 
+
+
+<?php
+     // method 1 
+     $author = array("charles", "Ssounyadip");
+    
+
+    print_r($author); ////Array ( [0] => charles [1] => Ssounyadip )
+     // method 2 
+	    echo "<br>";
+     $author =["charles", "Ssounyadip"];
+    
+
+    print_r($author);  ////Array ( [0] => charles [1] => Ssounyadip )
+   
+?>
