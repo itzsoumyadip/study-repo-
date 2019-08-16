5a01181404fb2c4d8aaf4b4bc9f3f37a83acb303
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-16 13:25:17 +0530

    strinReplace

---
+++ PHP/P code/stringReplaced.php
@@ -0,0 +1,6 @@
+<?
+$quote = "To be or not to be ? That is the question ";
+$replaced = str_replace("be","know",$quote); // str_replace("value to replace","value with which to replaced",string variable that are going to search )
+
+echo $replaced;//To know or not to know ? That is the question
+?>
