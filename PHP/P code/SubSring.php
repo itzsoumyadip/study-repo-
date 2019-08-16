38f81f0cf0fddee28420796dab49ba9c07d68334
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-16 13:43:57 +0530

    sUB STRING RETURNS PART OF A STRING

---
+++ PHP/P code/SubSring.php
@@ -0,0 +1,12 @@
+<?
+$quote = "To be or not to be ? That is the question";
+echo substr($quote,4);///e or not to be ? That is the question
+echo "<br>";
+echo substr($quote,-4);//tion
+echo "<br>";
+echo substr($quote,3,5);//be or
+echo "<br>";
+echo substr($quote,3,-5);//be or not to be ? That is the que
+
+
+?>
