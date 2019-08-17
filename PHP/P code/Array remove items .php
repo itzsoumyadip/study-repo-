dc29b37bef108d08fa4634a2a0d1c8f290e22168
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-17 23:50:07 +0530

    Array remove items  from back

---
+++ PHP/P code/Array remove items .php
@@ -0,0 +1,9 @@
+<?php
+     $author = array("charles", "sounyadip");
+    
+    $lastAurthor=array_pop($author);
+	echo $lastAurthor."<Br>";//sounyadip
+	 print_r($author);//Array ( [0] => charles )
+	
+	
+?>
