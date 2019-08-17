2d661fd03ce65389f9b700108dcdeb8204127860
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-17 23:30:38 +0530

    Array Value exit or not.php

---
+++ PHP/P code/Array Value exit or not.php
@@ -0,0 +1,15 @@
+<?php
+    
+     $author = array("charles", "Ssounyadip");
+      echo in_array('charles',$author); //1
+    	
+	
+	
+	// /third parameter in this in_array();  function 
+	 
+	   echo "<br>";
+	
+	      echo in_array('charles',$author,true);//1
+	
+?>
+	
