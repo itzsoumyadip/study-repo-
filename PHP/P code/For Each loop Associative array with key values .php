ae5fa0a05f8159d77e3334337c15e1c26d5d30d5
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-18 18:09:28 +0530

    For Each loop Associative array with key values .php

---
+++ PHP/P code/For Each loop Associative array with key values .php
@@ -0,0 +1,21 @@
+<?
+
+$authorAssociative =array (
+
+"poetic"=> "Willam",
+"brillint"=>"Jane",
+"query"=> "Charles"
+);
+
+
+ foreach ($authorAssociative  as $temp =>$val)
+   echo $val."(".$temp.")<br>";
+   /*
+ 
+   Willam(poetic)
+Jane(brillint)
+Charles(query)
+   
+   
+   */
+?>
