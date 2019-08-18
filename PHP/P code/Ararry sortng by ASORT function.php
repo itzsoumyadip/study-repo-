1f1753d8031ce77efc7e84840e5ee3291684aeb7
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-18 17:13:08 +0530

    Array sorting  by ASORT function .php

---
+++ PHP/P code/Ararry sortng by ASORT function.php
@@ -0,0 +1,15 @@
+<?
+
+$authorAssociative =array (
+
+"poetic"=> "Willam",
+"brillint"=>"Jane",
+"query"=> "Charles"
+);
+
+asort($authorAssociative);
+
+print_r($authorAssociative);//Array ( [query] => Charles [brillint] => Jane [poetic] => Willam )
+
+
+?>
