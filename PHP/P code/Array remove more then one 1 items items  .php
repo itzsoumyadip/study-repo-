ea0bbfedbe6991be05be3113b2c8f956d93e90a8
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-18 13:30:55 +0530

    Array remove more item  using one function .php and delete hole array

--- PHP/P code/Array remove more then one 1 items items  .php
+++ PHP/P code/Array remove more then one 1 items items  .php
@@ -5,7 +5,10 @@
     
 	  echo "before delete <br>". $author; //
 	 print_r($author);//ArrayArray ( [0] => charles [1] => sounyadip [2] => willam [3] => Mark Twain )
-	echo " <br> affter delete <br>";
+	echo " <br> after delete <br>";
  	unset($author[2],$author[0]); //Array ( [1] => sounyadip [3] => Mark Twain )
 	print_r($author);
+	echo " <br> and after delete hole array <br>";
+	unset($author);
+	
 	?>
