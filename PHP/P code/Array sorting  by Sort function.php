ed718ba391ef502a5a98f6598812611102e47fb0
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-18 17:00:10 +0530

    Array sorting  by Sort function.php

---
+++ PHP/P code/Array sorting  by Sort function.php
@@ -0,0 +1,16 @@
+<?
+
+
+$authors = array(
+
+                "Charles",
+				"Jane",
+				"Willam",
+				"Mark Twain",
+				"Louisa"
+);
+
+
+sort($authors);
+
+print_r($authors);// Array ( [0] => Charles [1] => Jane [2] => Louisa [3] => Mark Twain [4] => Willam )
