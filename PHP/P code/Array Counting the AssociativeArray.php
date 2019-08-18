a4eb7ca752723cad17190fab4718d9a4ba8ae22c
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-18 17:40:03 +0530

    Array Counting Associative array  the.php

---
+++ PHP/P code/Array Counting the AssociativeArray.php
@@ -0,0 +1,14 @@
+<?
+
+$authorAssociative =array (
+
+"poetic"=> "Willam",
+"brillint"=>"Jane",
+"query"=> "Charles"
+);
+
+
+echo count($authorAssociative);// AND echo count($authorAssociative,COUNT_RECURSIVE); AND echo count($authorAssociative,1);  
+  // a
+
+?>
