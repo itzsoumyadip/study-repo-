166a3d9ecb55cded4397f8dd1c1623379675e0b4
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-18 17:48:34 +0530

    For each loop .php

---
+++ PHP/P code/For Each loop.php
@@ -0,0 +1,26 @@
+<?
+
+
+$authors = array(
+
+                "Charles",
+				"Jane",
+				"Willam",
+				"Mark Twain",
+				"Louisa"
+);
+
+ foreach ($authors as $val)
+   echo $val."<br>";
+   /*
+   Charles
+   Jane
+   Willam
+   Mark Twain 
+   Louisa
+    
+   
+   
+   
+   */
+?>
