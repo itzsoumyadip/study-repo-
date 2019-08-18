e9fadf12f18a956917c21adbed35f78d6446f114
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2019-08-18 17:03:59 +0530

    Array sorting  by Sort function Reverse way.php

---
+++ PHP/P code/Array sorting  by Sort function Reverse way.php
@@ -0,0 +1,18 @@
+<?
+
+
+$authors = array(
+
+                "Charles",
+				"Jane",
+				"Willam",
+				"Mark Twain",
+				"Louisa"
+);
+
+
+rsort($authors);
+
+print_r($authors);// Array ( [0] => Willam [1] => Mark Twain [2] => Louisa [3] => Jane [4] => Charles )
+
+?>
