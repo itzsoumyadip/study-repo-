bea0e0f66e95f00c3529ed25c38941e745967767
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-01-19 11:32:54 +0530

    tuple is  immutable

---
+++ python/tuple.py
@@ -0,0 +1,14 @@
+#   tuple imMuteable  
+
+
+tup = (21,36,14,25,21) # is in round baracket 
+
+print(tup[1])
+  #36
+
+# since tuple is immutable interation speed is much faster the list
+print(tup.count(21))   #return the number of occuerence of the value
+#2 
+
+print(tup.index(14))  # find the index number of the value 
+# 2          
