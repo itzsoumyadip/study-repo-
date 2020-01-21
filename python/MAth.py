d4d78d14d520815567b448e9de47b1aaefed928b
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-01-21 20:04:58 +0530

    math method

---
+++ python/MAth.py
@@ -0,0 +1,20 @@
+import math
+from math import sqrt,pow  
+import math as m        # m use as alias name 
+
+x=math.sqrt(15)  #  Return the square root of x.   , def sqrt(x: SupportsFloat)
+print(x)  # 3.872983346207417
+
+print(math.floor(2.9)) # Return the floor(lowest value) of x as an Integral. 
+#2
+
+print(math.ceil(2.1)) #Return the ceiling(highest value) of x as an Integral.
+#3
+
+print(math.factorial(6))  # factorial 
+
+#** power function and + manual method ** # 
+print(3**2) # 9   
+print(math.pow(3,2))# 9.0
+
+# ***** #
