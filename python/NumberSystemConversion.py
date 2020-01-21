4d902bfbc7778c7f9f4b1fdcd55c283a08fd493b
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-01-21 13:22:08 +0530

    Number system Conversion

---
+++ python/NumberSystemConversion.py
@@ -0,0 +1,9 @@
+c=bin(25)  # Return the binary representation of an integer.
+print(c)   # 0b11001
+
+print(0b11001) # convert binary to decimal 
+
+print(oct(25)) #Return the octal representation of an integer.
+# 0o31
+print(hex(25)) # Return the hexadecimal representation of an integer. 
+# 0x19
