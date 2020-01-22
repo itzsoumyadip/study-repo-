f99db3ef1c76e80e50b2c4f38720f23102001ea6
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-01-22 20:19:29 +0530

    data types

---
+++ python/datatypes.py
@@ -0,0 +1,10 @@
+a =  {'rahul':'samsung', 'kiran':'Iphone','Dev': 'Oneplus'}
+print(a.keys())
+  # dict_keys(['rahul', 'kiran', 'Dev'])
+print(a.values())  
+  # dict_values(['samsung', 'Iphone', 'Oneplus'])
+  # ** access value of Dictionary
+print(a['rahul'])  
+#samsung
+print(a.get('kiran'))
+#Iphone
