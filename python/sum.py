4f5d4773ec94b862a16baafe841c101da75b7a08
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-01-22 16:17:37 +0530

    taking input from user and evaluate an expression

--- python/sum.py
+++ python/sum.py
@@ -1,6 +1,11 @@
-x=input("ENTER st NUMBER")
+x=input("ENTER 1st NUMBER")
 y=input("ENTER 2nd NUMBER")
 x=int(x)
 y=int(y)
 z=x+y
 print(z)  
+
+# another way 
+result = eval(input('enter en expr'))  # enter expression --->  2 + 6 - 1
+print(result)
+
