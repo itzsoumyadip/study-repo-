1c5404497ea4c0b33dfa073d863986e50324fae5
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-01-22 16:42:20 +0530

    char type

---
+++ python/Char.py
@@ -0,0 +1,24 @@
+# python does not support CHAR type data types 
+
+
+ch = input('enter a char')[0]
+print(ch)
+# input ---- pqr
+#output ---- p 
+print(type(ch))
+# <class 'str'>
+
+# equivalent to this code 
+
+ch = input('enter a char')
+print(ch[0])
+
+# input ---- pqr
+#output ---- p 
+print(type(ch))
+# <class 'str'>
+
+
+
+
+
