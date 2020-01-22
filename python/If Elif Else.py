589e2600c83eceba9c5be3dac2a6683cb67ddf19
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-01-22 20:45:46 +0530

    if  else if  else

---
+++ python/If Elif Else.py
@@ -0,0 +1,10 @@
+x =6
+if x==3:
+    print('3')
+elif x==4:
+    print('4')
+elif x==5:
+    print('5') 
+else:
+    print('wrong input')
+       
---
+++ python/If else.py
@@ -0,0 +1,19 @@
+x=8
+r=x%2 
+
+
+if r==0:           # use of  round bracket is optional in python 
+    print('even')         # in python Codeblocks knows as Suite
+    if x>5:               # to keep next line inside if block ,use same indentation like the 1st line of if statement block
+        print('Number is greater then 5') 
+    else:
+        print('small')
+else:
+    print("odd")
+    
+                
+
+
+
+
+                # indentation
