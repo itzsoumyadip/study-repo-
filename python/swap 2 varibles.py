8d1a8fcbc4e76acab7caba77bdcb709fab256575
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-01-23 21:30:41 +0530

    swap ing in  python style

---
+++ python/swap 2 varibles.py
@@ -0,0 +1,15 @@
+# fo Python only 
+
+a=5
+b=6
+
+a,b=b,a     # store in stack then rotate two value by --> # ROT_TWO() --> SWAPs the two top most stack items.
+
+print(a,b)
+
+
+
+
+###   useing 3rd varible  consume  extra memory 
+## using arithmetic consusme extra bit memory 
+## using xor is favorable  
