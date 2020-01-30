4d81a9f459c28cd6a4f2f75f8530bf1fdd3a27cb
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-01-30 15:55:45 +0530

    factorial

--- python/factorial.py
+++ python/factorial.py
@@ -1,15 +1,17 @@
 def factorial(n):
     f=1
-    for i in range(1,n+1):
+    for i in range(1,n+1): # 1 to n+1 (if 4 +1=5)for rannge() method 
         f=f*i
     
     return f
 
 x=4 
 
-result= factorial(x)
+result =factorial(x)
 
 
 print(result)
+
+## or  use math.factorial()
 
 
