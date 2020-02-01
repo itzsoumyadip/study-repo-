caa7da21729952c27436f7c973b0fc0ac8b80f17
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-02-01 20:54:47 +0530

    class in python

---
+++ python/OPP/Class .py
@@ -0,0 +1,21 @@
+class Computer:          # class 
+    def config(self):         ## attributes   # self is the object which are passing 
+        print('from class computer')   # behaviour or method 
+
+com=Computer()        ##com is the instance of class  computer  ,and  computer()
+com2=Computer()
+
+com.config()    # behind the  scene config() will take com as parameter and pass it like config(com)
+# from class computer
+
+## another way of calling class function 
+Computer.config(com)   #oject com is pass as argument
+#from class computer
+
+
+com2.config()
+#from class computer 
+
+
+print(type(com))
+# <class '__main__.Computer'>   
