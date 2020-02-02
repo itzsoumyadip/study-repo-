d619976bd15b2d7f2c8149c174f7d5fd65df499e
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-02-02 16:39:19 +0530

    __init__ method in python

---
+++ python/OPP/__init__ method.py
@@ -0,0 +1,28 @@
+# All classes have a function called __init__(), which is always executed when the class is being initiated.
+# Use the __init__() function to assign values to object properties, or other operations that are necessary to do when the object is being created:
+
+
+
+class computer:
+ # Note: The __init__() function is called automatically every time the class is being used to create a new object.
+    def __init__(self,argu1,argu2):
+        self.argu1 = argu1
+        self.argu2 =  argu2
+
+    def config(self):
+        print(' config is ', self.argu1 ,self.argu2)
+    def setter(self,argu,argu2):
+        self.argu1= argu
+        self.argu2 =argu2
+
+
+
+com=computer('intel','i5')     # value is pass in __init__() method  like constructor
+com.config()
+ #config is  intel i5
+com2=computer('ryzen',5)
+com2.config()
+ #config is  ryzen 5
+com.setter('intel','i7')
+com.config()
+ #config is  intel i7
