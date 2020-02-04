d69464a21bfa1d550762832bdebb8d9bcbb01fda
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-02-04 13:13:20 +0530

    Passing object as a argument

---
+++ python/OPP/object as argument .py
@@ -0,0 +1,31 @@
+class Person:          
+    def __init__(self,argu1,argu2):
+        self.argu1 = argu1
+        self.argu2 =  argu2
+   
+   
+    def compare(self,other):      
+         if self.argu2==other.argu2:
+            return True
+         else:
+            return False   
+
+ 
+        
+
+
+  
+       
+
+
+P1=Person('navin',23)
+P2=Person('rishi',25)
+
+
+if P1.compare(P2):  
+    print('they are same')
+else:
+    print('they are not same')    
+ 
+
+ #they are not same
