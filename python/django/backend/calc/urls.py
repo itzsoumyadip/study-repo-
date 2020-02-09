a7f33f2f8c46b8e7e906e7fc09b622b54b81c263
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-02-09 21:32:37 +0530

    urls and views  and how to use class in view

--- python/django/backend/calc/urls.py
+++ python/django/backend/calc/urls.py
@@ -4,5 +4,8 @@ from . import views
  
 
 urlpatterns = [
-    path('',views.home,name='home'),
+#    path('',views.home,name='home'),
+    path('',views.ListView.as_view(),name="home"),
+    path('add',views.add,name='add'),
+  
 ] 
--- python/django/backend/calc/views.py
+++ python/django/backend/calc/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 # Create your views here.
-def home(request):
-    return render(request,'home.html',{'name':'itzsoumyadip'})
+
+# def home(request):
+#     return render(request,'home.html',{'name':'itzsoumyadip'})
+
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    val3 = val1+val2   
+    return render(request,"result.html",{'res':val3})
+
+
+class ListView(TemplateView):
+    template_name ="home.html " 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        listt =[1,2,3,4,5,6]
+        context['lis'] = listt 
+        return context
+     
+       
+     
+     
+     
+     
+
+
+## work with class
+
