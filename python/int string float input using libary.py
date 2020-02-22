4e4561bd590d0681494c51e7196fa44215064426
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-02-22 17:05:30 +0530

    int string float input using libary.py

---
+++ python/int string float input using libary.py
@@ -0,0 +1,21 @@
+import sys 
+sys.path.append('/Users/hacky/Desktop/Online downlad/code/study-repo--master/python/libary')
+
+from cs50 import get_int,get_string,get_float 
+
+
+inte =get_int("enter a number")
+st =get_string('enter a string')
+floa=get_float('enter a float')
+print(inte)
+print(st)
+print(floa)
+
+'''
+enter a number56
+enter a stringdssa
+enter a float893.36
+56
+dssa
+893.36
+'''
