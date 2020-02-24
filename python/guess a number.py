7a6d178776da6eea7baabf062b8be0970e9adf27
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-02-24 20:33:35 +0530

    guessing a number

---
+++ python/guess a number.py
@@ -0,0 +1,23 @@
+import sys 
+sys.path.append('/Users/hacky/Desktop/Online downlad/code/study-repo--master/python/libary')
+
+import random
+from cs50 import get_int
+
+secretNumber =random.randint(1,5)   # a random number will be genarate
+
+for i in range(1,7):   # number of times user will get chance is 6
+        guessnumber = get_int("guess a number")  # take a number from user 
+        if guessnumber < secretNumber:
+            print("no you are wrong : you have enter smaller number  try gessing again")
+        elif guessnumber > secretNumber:
+            print("no you are wrong : you have enter greater number  try gessing again")
+        else: 
+            break  # if the user guess the correct number 
+         
+if guessnumber == secretNumber:
+    print("YEA! you guess is 100% correct Number is: " + str(guessnumber) + " indeed. you have taken "+ str(i)+ " times of times to guess it" )        
+else:
+    print("your guessing number is: "+ str(secretNumber))  # if user unable to guess the nuber
+
+
