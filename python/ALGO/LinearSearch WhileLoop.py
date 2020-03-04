54ff71b00c9f00380ef436716e532c75bd992fb1
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-03-04 18:29:12 +0530

    liner search

--- python/ALGO/LinearSearch WhileLoop.py
+++ python/ALGO/LinearSearch WhileLoop.py
@@ -8,7 +8,7 @@ pos = 0  #for storing the searched  number's index
 
 list = [9,8,7,6,5,4,3,2,1,98,97,87,64,31,25,58,36,69,14,47,74,85,96,63,41,75,95,35,15,91,73,67,68,62]
 
-
+print(len(list))
 n=get_int("enter a number to search :")  # this number will be search  
 
 
@@ -28,7 +28,7 @@ def Linear(list,n):          # liner Search method
 
 
 
-if Linear(list,n):                # if the number is found in the list 
+if Linear(list,n):       # function calling in if          # if the number is found in the list 
     print("Found: "+ str(n) + " at position: " +str(pos+1))
 else:                           # if number is not found 
     print(f'Sorry :{n} is not present in the list')
