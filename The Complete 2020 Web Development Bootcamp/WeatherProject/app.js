d1d4ff2a3ac6472a56e8668972d27d7f859f510e
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-05-06 17:20:00 +0530

    WEATHER  APP WITH openweather https://openweathermap.org/  API

--- The Complete 2020 Web Development Bootcamp/WeatherProject/app.js
+++ The Complete 2020 Web Development Bootcamp/WeatherProject/app.js
@@ -10,9 +10,20 @@ app.get("/",function(req,res){
 
     const url="https://api.openweathermap.org/data/2.5/weather?q=Kolkata,india&appid=6ad43b72b31a2d4fe48a12e69f073aa5&units=metric"
 
-    https.get(url,function(response){
-            console.log(response.statusCode)
-          
+    https.get(url,function(response){   //take the string json data from url and pass to the response
+                    
+        response.on('data',function(data) {
+            const WeatherObj=JSON.parse(data)  //converting string json data to js object
+            const LOCATION = WeatherObj.name
+            const country  =WeatherObj.sys.country
+            const icon    = WeatherObj.weather[0].icon
+            res.write("<h1>LOCATION: "+LOCATION+" Country: " +country +"</h1>" );
+            res.write("<p> Description :"+ WeatherObj.weather[0].main+"</p>")
+            res.write("<h1>Temparature "+WeatherObj.main.temp +"C and Feel like temparature: "+WeatherObj.main.feels_like+"</h1>")
+            res.write("<img src= http://openweathermap.org/img/wn/"+icon+"@2x.png>")  // image code 
+            res.write("<p><a href=\"https://openweathermap.org/weather-conditions\"> OPEN WEATHER API</a></p> ")
+            res.send(); 
+        })
 
     }) 
 
