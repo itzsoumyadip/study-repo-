f0502d1e81a84a467714090e42500107debaf3de
itzsoumyadip <29505740+itzsoumyadip@users.noreply.github.com>
2020-06-29 21:37:45 +0530

    first 10 natural number and reverse them in java

---
+++ java/p code/RTenNN.java
@@ -0,0 +1,10 @@
+public class RTenNN {
+
+    public static void main(String [] args){
+        
+        for(int i=10;i>=1;i--){
+            System.out.print(" "+i);
+        }
+    }
+    
+}
---
+++ java/p code/TenNN.java
@@ -0,0 +1,9 @@
+public class TenNN {
+    
+    public static void main(String [] args){
+        
+        for(int i=1;i<=10;i++){
+            System.out.print(" "+i);
+        }
+    }
+}
